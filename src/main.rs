03caeb27ba2b5f837f3c22bdffffb5be09a7db98
Marco Napetti <nappa85@therocktrading.com>
2021-09-16 09:32:28 +0200

    Worse situation

--- src/main.rs
+++ src/main.rs
@@ -36,23 +36,23 @@ async fn get_conn() -> DatabaseConnection {
     Database::connect(&url).await.unwrap()
 }
 
-async fn do_something_with_ref<'a, C, I>(_conn: &C, iter: I) -> Result<HashMap<String, Money>, ()>
+async fn do_something_with_ref<'a, C, I>(_conn: &C, iter: I) -> Result<HashMap<&'a str, Money>, ()>
 where
     C: ConnectionTrait,
     I: Iterator<Item=&'a Yadda>,
 {
-    Ok(iter.map(|y| (y.currency.clone(), y.amount)).collect())
+    Ok(iter.map(|y| (y.currency.as_str(), y.amount)).collect())
 }
 
-async fn do_something_else_with_ref<'a, C, I>(_conn: &C, iter: I) -> Result<HashMap<String, Money>, ()>
+async fn do_something_else_with_ref<'a, C, I>(_conn: &C, iter: I) -> Result<HashMap<&'a str, Money>, ()>
 where
     C: ConnectionTrait,
     I: Iterator<Item=&'a Yadda>,
 {
-    Ok(iter.map(|y| (y.currency.clone(), y.amount)).collect())
+    Ok(iter.map(|y| (y.currency.as_str(), y.amount)).collect())
 }
 
-async fn transaction_nightmare(yaddayadda: &[Yadda]) -> Result<HashMap<String, Money>, ()> {
+async fn transaction_nightmare<'a>(yaddayadda: &'a [Yadda]) -> Result<HashMap<&'a str, Money>, ()> {
     let conn = get_conn().await;
     let (foo, bar) = conn.transaction::<_, _, VoidError>(|txn| Box::pin(async move {
         let foo = do_something_with_ref(txn, yaddayadda.iter()).await?;
